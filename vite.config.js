a30e127efa41e60722501feff583c7043555f692
Rejown <rejownahmed@yahoo.com>
2022-09-25 22:28:01 +0600

    u

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import libCss from 'vite-plugin-libcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
@@ -18,5 +19,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()]
+  plugins: [vue(), libCss()]
 })
