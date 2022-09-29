836497b2f02859f0b6dac93475720fb31cf537a7
Rejown <rejownahmedzisan@gmail.com>
2022-09-30 02:38:53 +0600

    Buffer display and custom text added

--- src/components/customRangeController.vue
+++ src/components/customRangeController.vue
@@ -1,32 +1,6 @@
-<template>
-    <div 
-        class="range-container"
-        :class="props.expandOnHover ? 'expandOnHover' : ''"
-        :style="{   
-            '--progress-position': progressPosition, 
-            '--range-container-height': props.rangeContainerHeight, 
-            '--range-height': props.rangeHeight, 
-            '--thumb-height': props.thumbHeight, 
-            '--range-bg-color': props.rangeBgColor, 
-            '--thumb-color': props.thumbColor, 
-            '--preview-color': props.previewColor, 
-            '--progress-color': props.progressColor, 
-        }"
-        @mousemove="handlerangeUpdate"
-        @mousedown="toggleScrubbing"
-        @mouseup="isScrubbing = false"
-        @mouseleave="isScrubbing = false">
-
-        <div class="range">
-            <span v-if="!props.noPreviewBar" class="preview"></span>
-            <span v-if="!props.noProgressBar" class="progress"></span>
-            <span v-if="!props.noThumb" class="thumb-indicator" :style="props.alwaysShowThumb ? {display: 'block'} : ''"></span>
-        </div>
-    </div>
-  </template>
-  <script setup>
+<script setup>
     import { ref } from 'vue'
-    
+        
     const props = defineProps(
         {
             modelValue: {
@@ -34,6 +8,14 @@
                 default: 0
             },
             alwaysShowThumb: {
+                type: Boolean,
+                default: false
+            },
+            showBuffer: {
+                type: Boolean,
+                default: false
+            },
+            showCustomText: {
                 type: Boolean,
                 default: false
             },
@@ -67,7 +49,7 @@
             },
             rangeBgColor: {
                 type: String,
-                default: 'rgba(100, 100, 100, .5)'
+                default: 'rgba(100, 100, 100, 0.5)'
             },
             thumbColor: {
                 type: String,
@@ -81,11 +63,26 @@
                 type: String,
                 default: 'red'
             },
+            customTextColor: {
+                type: String,
+                default: '#fff'
+            },
+            customTextFontSize: {
+                type: String,
+                default: '12px'
+            },
+            bufferColor: {
+                type: String,
+                default: 'rgba(255, 255, 255, 0.4)'
+            },
         }
     )
     const emit  = defineEmits(['update:modelValue'])//emit data to v-model
     const isScrubbing = ref(false)//Not clicked/dragged
-    const progressPosition = ref(props.modelValue)//setting it to modelValue gives us more flexibility over default positioning
+    //setting it to modelValue gives us more flexibility over default positioning
+    const progressPosition = ref(props.modelValue)
+    const customTextValue = ref('0%')//Percent Value To Show
+    const bufferPosition = ref(0)//show how much buffered (If wish to)
 
     const toggleScrubbing = (e)=> {
         if( props.modelValue == null ) alert(msg.value)
@@ -102,8 +99,8 @@
             emit('update:modelValue', percent)//Emit the current position to v-model directive
         }
     }
-    
-    defineExpose({ progressPosition }) //Expose the porgressPosition Ref
+
+    defineExpose({ progressPosition, bufferPosition, customTextValue }) //Expose the porgressPosition Ref
     /*
     * This is a must needed since sometimes we'll need to update 
     * the position from it's parent
@@ -111,7 +108,42 @@
     * the audio/video plays
     * See the video player example created with it for more details
     */
-  </script>
+</script>
+<template>
+    <div 
+        class="range-container"
+        :class="props.expandOnHover ? 'expandOnHover' : ''"
+        :style="{
+            '--buffer-position': bufferPosition,    
+            '--percent-font-size': props.customTextFontSize,    
+            '--percent-color': props.customTextColor,    
+            '--progress-position': progressPosition, 
+            '--range-container-height': props.rangeContainerHeight, 
+            '--range-height': props.rangeHeight, 
+            '--thumb-height': props.thumbHeight, 
+            '--range-bg-color': props.rangeBgColor, 
+            '--thumb-color': props.thumbColor, 
+            '--preview-color': props.previewColor, 
+            '--progress-color': props.progressColor, 
+            '--buffer-color': props.bufferColor, 
+        }"
+        @mousemove="handlerangeUpdate"
+        @mousedown="toggleScrubbing"
+        @mouseup="isScrubbing = false"
+        @mouseleave="isScrubbing = false">
+
+        <div class="range">
+            <span v-if="!props.noPreviewBar" class="preview"></span>
+            <span v-if="props.showBuffer" class="buffered"></span>
+            <span v-if="!props.noProgressBar" class="progress">
+                <span v-if="showCustomText" class="percent-value">{{customTextValue}}</span>
+            </span>
+            <slot></slot>
+            <span v-if="!props.noThumb" class="thumb-indicator" :style="props.alwaysShowThumb ? {display: 'block'} : ''"></span>
+        </div>
+    </div>
+  </template>
+
   <style lang="scss" scoped> 
     .range-container {
         --range-container-height: 7px;
@@ -120,8 +152,9 @@
         --thumb-height: 200%;
         --range-bg-color: rgba(100, 100, 100, .5);
         --thumb-color: red;
-        --preview-color: rgb(150, 150, 150);
-        --progress-color: red;
+        --preview-color: rgba(255,255,255,.5);
+        --progress-color: #f00;
+        --buffer-color: rgba(255,255,255,.3);
         &.expandOnHover{
             --hover-range-height: 100%;
         }
@@ -155,6 +188,26 @@
                 height: 100%;
                 right: calc(100% - var(--progress-position) * 100%);
                 background-color: var(--progress-color);
+                text-align: center;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                .percent-value{
+                    font-size: var(--percent-font-size);
+                    color: var(--percent-color);
+                    z-index: 1080;
+                }
+            }
+            .buffered {
+                content: "";
+                position: absolute;
+                left: 0;
+                top: 0;
+                bottom: 0;
+                height: 100%;
+                right: calc(100% - var(--buffer-position) * 100%);
+                transition: all 100ms ease-in-out;
+                background-color: var(--buffer-color);
             }
             .thumb-indicator{
                 display: none;
@@ -165,7 +218,6 @@
                 left: calc(var(--progress-position) * 100%);
                 background-color: var(--thumb-color);
                 border-radius: 50%;
-                transition: transform 150ms ease-in-out;
                 aspect-ratio: 1 / 1;
                 z-index: 1080;
             }
@@ -184,4 +236,3 @@
         } 
     }
   </style>
-  
